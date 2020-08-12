c215ef001b9e745d2b0985e21ab45eca06bd5c38
Ken Lerdahl <kenlerdahl@Kens-MacBook-Pro.local>
2020-08-12 14:48:09 -0500

    Added wireFrame folder, created home controller, began adding nav

--- src/main/java/com/dinnercircle/dinnercircle/controllers/HomeHontroller.java
+++ src/main/java/com/dinnercircle/dinnercircle/controllers/HomeHontroller.java
@@ -1,4 +1,13 @@
 package com.dinnercircle.dinnercircle.controllers;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeHontroller {
+
+    public String index() {
+        return "index";
+    }
+
 }
