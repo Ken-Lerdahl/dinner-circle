78bafe6a1f73356f0fffa1c925df5133cea36250
Ken Lerdahl <ken.lerdahl@gmail.com>
2020-09-29 06:54:54 -0500

    Lots of polish and consistency changes.

--- src/main/java/com/dinnercircle/dinnercircle/controllers/HomeController.java
+++ src/main/java/com/dinnercircle/dinnercircle/controllers/HomeController.java
@@ -1,15 +1,26 @@
 package com.dinnercircle.dinnercircle.controllers;
 
 
+import com.dinnercircle.dinnercircle.models.SearchRepository;
+import com.dinnercircle.dinnercircle.models.User;
+import com.dinnercircle.dinnercircle.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @RequestMapping("home")
-    public String displayHome() {
+    public String displayHome(Model model) {
+
+        User currentUser = SearchRepository.getCurrentUser(userRepository);
+        model.addAttribute("welcome", "Welcome back, " + currentUser.getUsername() + "!");
         return "/home";
     }
 
